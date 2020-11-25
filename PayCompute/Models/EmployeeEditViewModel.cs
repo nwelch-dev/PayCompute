fae48ca0e61efeaf60cf52a82210ef7a9048465e
Nic Welch <nprsnw@protonmail.com>
2020-11-25 15:43:05 -0600

    Implemented Create/Edit View Models and updated EmployeeController. Database Migration update for adding Phone number

--- PayCompute/Models/EmployeeEditViewModel.cs
+++ PayCompute/Models/EmployeeEditViewModel.cs
@@ -8,12 +8,12 @@ using System.Threading.Tasks;
 
 namespace PayCompute.Models
 {
-    public class EmployeeCreateViewModel
+    public class EmployeeEditViewModel
     {
         public int Id { get; set; }
-        [Required(ErrorMessage ="Employee Number is required"), RegularExpression("^$[A-Z], {3,3}, [0-9] {3}$")]
+        [Required(ErrorMessage = "Employee Number is required"), RegularExpression("^$[A-Z], {3,3}, [0-9] {3}$")]
         public string EmployeeNo { get; set; }
-        [Required(ErrorMessage ="First name is required"), StringLength(50, MinimumLength =2)]
+        [Required(ErrorMessage = "First name is required"), StringLength(50, MinimumLength = 2)]
         [RegularExpression(@"^[A-Z][a-zA-Z""'\s-]*$")]
         [Display(Name = "First Name")]
         public string FirstName { get; set; }
@@ -23,22 +23,15 @@ namespace PayCompute.Models
         [RegularExpression(@"^[A-Z][a-zA-Z""'\s-]*$")]
         [Display(Name = "Last Name")]
         public string LastName { get; set; }
-        public string FullName
-        {
-            get
-            {
-                return FirstName + (string.IsNullOrEmpty(MiddleName) ? " " : (" " + (char?)MiddleName[0] + ".").ToUpper() + LastName);
-            }
-        }
         public string Gender { get; set; }
         [Display(Name = "Photo")]
         public IFormFile ImageUrl { get; set; }
         [DataType(DataType.Date), Display(Name = "Date Of Birth")]
         public DateTime DOB { get; set; }
         [DataType(DataType.Date), Display(Name = "Date Joined")]
-        public DateTime DateJoined { get; set; } = DateTime.UtcNow;
+        public DateTime DateJoined { get; set; }
         public string Phone { get; set; }
-        [Required(ErrorMessage ="Job Role is required"), StringLength(100)]
+        [Required(ErrorMessage = "Job Role is required"), StringLength(100)]
         public string Designation { get; set; }
         [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
@@ -46,17 +39,17 @@ namespace PayCompute.Models
         [Required, StringLength(50), Display(Name = "NI No./SSN")]
         [RegularExpression(@"^[A-CEGHJ-PR-TW-Z] {1} [A-CEGHJ-NPR-TW-Z] {1} [0-9] {6} [A-D\s]$")]
         public string NationalInsuranceNo { get; set; }
-        [Display(Name ="Payment Method")]
+        [Display(Name = "Payment Method")]
         public PaymentMethod PaymentMethod { get; set; }
-        [Display(Name ="Student Loan")]
+        [Display(Name = "Student Loan")]
         public StudentLoan StudentLoan { get; set; }
-        [Display(Name ="Union Member")]
+        [Display(Name = "Union Member")]
         public UnionMember UnionMember { get; set; }
         [Required, StringLength(150)]
         public string Address { get; set; }
         [Required, StringLength(50)]
         public string City { get; set; }
-        [Required, StringLength(50), Display(Name ="Post Code")]
+        [Required, StringLength(50), Display(Name = "Post Code")]
         public string Postcode { get; set; }
     }
 }
